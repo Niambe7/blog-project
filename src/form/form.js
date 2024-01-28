ecf2ac2b10bfd374771108e91e010b48d0b47616
Niambe <niambegui785@gmail.com>
2024-01-28 20:40:11 +0100

    Ajout d'un nouveau blog vers l'api

--- src/form/form.js
+++ src/form/form.js
@@ -1,3 +1,57 @@
 import "../assets/styles/styles.scss";
 import "./form.scss";
-    
+
+const form = document.querySelector("form")
+const errorElement = document.getElementById("errors");
+
+let errors = [];
+
+form.addEventListener("submit", async (event)=>{
+    event.preventDefault();
+    errors = [];
+    const formData = new FormData(form);
+    const article = Object.fromEntries(formData.entries());
+
+    if(formIsvalid(article)){
+
+        try{
+            const json = JSON.stringify(article);
+            const response = await fetch("https://restapi.fr/api/articles", {
+              method: "POST",
+              body: json,
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+            const body = await response.json();
+            console.log(body); 
+        }catch(error){
+            console.error("error: ",error)
+        }
+
+      
+    }
+
+})
+
+const formIsvalid = article =>{
+    if(!article.author || !article.category || !article.content){
+        errors.push("Veuillez remplir tous les champs svp")
+    } else{
+        errors = [];
+    } 
+
+    if(errors.length){
+        let errorHtml= "";
+        errors.forEach(error => {
+            errorHtml += `<li>${error}</li>` 
+        });
+
+        errorElement.innerHTML = errorHtml;
+        return false;
+        
+    } else{
+        errorElement.innerHTML = "";
+        return true;
+    }
+}
