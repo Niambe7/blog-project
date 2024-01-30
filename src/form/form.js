f47eb8b81bbd7d19b1ac8b78281c989651e9233a
Niambe <niambegui785@gmail.com>
2024-01-30 18:06:17 +0100

    Suppression d'un blog sur le DOM et sur l'API

--- src/form/form.js
+++ src/form/form.js
@@ -1,57 +1,53 @@
 import "../assets/styles/styles.scss";
 import "./form.scss";
 
-const form = document.querySelector("form")
-const errorElement = document.getElementById("errors");
-
+const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors");
 let errors = [];
 
-form.addEventListener("submit", async (event)=>{
-    event.preventDefault();
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const article = Object.fromEntries(formData.entries());
+  if (formIsValid(article)) {
+    try {
+      const json = JSON.stringify(article);
+      const response = await fetch("https://restapi.fr/api/article", {
+        method: "POST",
+        body: json,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const body = await response.json();
+      console.log(body);
+    } catch (e) {
+      console.error("e : ", e);
+    }
+  }
+});
+
+const formIsValid = (article) => {
+  if (
+    !article.author ||
+    !article.category ||
+    !article.content ||
+    !article.img ||
+    !article.title
+  ) {
+    errors.push("Vous devez renseigner tous les champs");
+  } else {
     errors = [];
-    const formData = new FormData(form);
-    const article = Object.fromEntries(formData.entries());
-
-    if(formIsvalid(article)){
-
-        try{
-            const json = JSON.stringify(article);
-            const response = await fetch("https://restapi.fr/api/articles", {
-              method: "POST",
-              body: json,
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-            const body = await response.json();
-            console.log(body); 
-        }catch(error){
-            console.error("error: ",error)
-        }
-
-      
-    }
-
-})
-
-const formIsvalid = article =>{
-    if(!article.author || !article.category || !article.content){
-        errors.push("Veuillez remplir tous les champs svp")
-    } else{
-        errors = [];
-    } 
-
-    if(errors.length){
-        let errorHtml= "";
-        errors.forEach(error => {
-            errorHtml += `<li>${error}</li>` 
-        });
-
-        errorElement.innerHTML = errorHtml;
-        return false;
-        
-    } else{
-        errorElement.innerHTML = "";
-        return true;
-    }
-}
+  }
+  if (errors.length) {
+    let errorHTML = "";
+    errors.forEach((e) => {
+      errorHTML += `<li>${e}</li>`;
+    });
+    errorElement.innerHTML = errorHTML;
+    return false;
+  } else {
+    errorElement.innerHTML = "";
+    return true;
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,72 @@
 import "./assets/styles/styles.scss";
 import "/index.scss"
 
-console.log("In Accueil")
+const createArticles = (articles)=>{
+const articleContainerElement = document.querySelector(".articles-container");
+
+const articlesDOM = articles.map((article) =>{
+    const articleDOM = document.createElement("div");
+    articleDOM.classList.add("article")
+    articleDOM.innerHTML = `
+            
+            <img
+              src="${article.img}"
+              alt="profile"
+            />
+            <h2>${article.title}</h2>
+            <p class="article-author">${article.author} - ${article.category}</p>
+            <p class="article-content">${article.content}</p>
+            <div class="article-actions">
+              <button class="btn btn-danger" data-id="${article._id}">Supprimer</button>
+            </div>
+    `;
+    
+    return articleDOM;
+
+})
+articleContainerElement.innerHTML = "";
+articleContainerElement.append(...articlesDOM);
+const deleteBTN = articleContainerElement.querySelectorAll(".btn-danger");
+
+console.log(deleteBTN);
+
+deleteBTN.forEach(button => {
+    button.addEventListener("click", async (event)=>{
+        let target = event.target;
+        let articleId = target.dataset.id;
+        console.log(articleId)
+
+       try{
+         const response = await fetch(
+           `https://restapi.fr/api/article/${articleId}`,
+           {
+             method: "DELETE"
+           }
+         );
+
+         const body = await response.json();
+         console.log(body)
+         fetchArticle();
+       }
+       catch(error){
+        console.error("error: ", error)
+       }
+    })
+});
+
+}
+
+
+const  fetchArticle = async()=>{
+    try{
+        const response = await fetch("https://restapi.fr/api/article");
+        const articles = await response.json();
+        console.log(articles)
+        createArticles(articles)
+    }catch(e){
+        console.error(e)
+    }
+
+}
+
+fetchArticle();
